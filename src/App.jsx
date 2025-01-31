e333e199ebb5f949fcd1bed50417f085ecd74122
Hyan <38265480+hyanmarcos@users.noreply.github.com>
2025-01-31 08:45:11 -0300

    `Added axios library and API call to fetch news data, updated App component to display news articles and added loading animation`

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,69 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Audio } from "react-loader-spinner";
 
 import "./styles/App.css";
 
 import { Navbar } from "../src/components/Navbar/Navbar";
 import { Article } from "./components/Article/Article";
 
-import articleImg from "../src/assets/images/Rectangle 30.png";
-import articleImg2 from "../src/assets/images/Rectangle 32.png";
-import articleImg3 from "../src/assets/images/Rectangle 34.png";
-import { Counter } from "./components/Counter/Counter";
+import axios from "axios";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Navbar />
-        <Counter />
-        <section id="articles">
-          <Article
-            thumbnail={articleImg}
-            title="Designing Dashboards"
-            provider="NASA"
-            description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-          />
-          <Article
-            thumbnail={articleImg2}
-            title="Vibrant Portraits of 2020"
-            provider="SpaceNews"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo neque in eveniet, iste quod quasi dolorum voluptate velit. Libero commodi quisquam ullam doloremque, asperiores sint consectetur ipsam voluptates cum facere."
-          />
-          <Article
-            thumbnail={articleImg3}
-            title="36 Days of Malayalam type"
-            provider="Spaceflight Now"
-            description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-          />
-          <Article
-            thumbnail={articleImg}
-            title="Designing Dashboards"
-            provider="NASA"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo neque in eveniet, iste quod quasi dolorum voluptate velit. Libero commodi quisquam ullam doloremque, asperiores sint consectetur ipsam voluptates cum facere."
-          />
-        </section>
-      </>
-    );
-  }
+function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function loadNews() {
+      const response = await axios.get(
+        "https://api.spaceflightnewsapi.net/v4/articles/"
+      );
+      const newsData = response.data.results;
+
+      console.log(newsData);
+      setNews(newsData);
+    }
+
+    loadNews();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+
+      <section id="articles">
+        {news.length === 0 ? (
+          <div
+            style={{
+              height: "400px",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Audio
+              height="80"
+              width="80"
+              radius="9"
+              color="white"
+              ariaLabel="loading"
+              wrapperStyle
+              wrapperClass
+            />
+          </div>
+        ) : (
+          news.map((article) => (
+            <Article
+              key={article.id}
+              title={article.title}
+              provider={article.newsSite}
+              description={article.summary}
+              thumbnail={article.image_url}
+            />
+          ))
+        )}
+      </section>
+    </>
+  );
 }
 
 export default App;
